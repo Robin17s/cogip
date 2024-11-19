6bf07910d553be04656451796abbf774aaba12bc
Robin Sanders <robin.sanders1@telenet.be>
2024-11-19 16:45:11 +0100

    Add User, UserController, UserService and UserRepository to classes

---
+++ src/main/java/org/becode/projects/CogipApplication.java
@@ -0,0 +1,13 @@
+package org.becode.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/becode/projects/CogipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.becode.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
