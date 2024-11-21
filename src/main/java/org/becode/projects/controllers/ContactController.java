5e2fde7d9bba76711ea2fee9f14923bb2f7c1c81
Robin Sanders <robin.sanders1@telenet.be>
2024-11-21 09:34:24 +0100

    Add crud methods to classes ContactService and ContactController

--- src/main/java/org/becode/projects/controllers/ContactController.java
+++ src/main/java/org/becode/projects/controllers/ContactController.java
@@ -1,5 +1,52 @@
 package org.becode.projects.controllers;
 
+import java.util.List;
+
+import org.becode.projects.domain.Company;
+import org.becode.projects.domain.Contact;
+import org.becode.projects.services.CompanyService;
+import org.becode.projects.services.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContactController {
 
+	@Autowired
+	private ContactService service;
+	
+	public ContactController() {
+		
+	}
+	
+	@GetMapping("/contacts")
+	public List<Contact> getAllContact(){
+		return service.getAllContacts();
+	}
+	
+	@GetMapping("/contacts/{id}")
+	public Contact getSpecificContact(@PathVariable int id) {
+		return service.getSpecificContact(id);
+	}
+	
+	@PostMapping("/contacts")
+	public String createNewContact(@RequestBody Contact contact) {
+		return service.createNewContact(contact);
+	}
+	
+	@DeleteMapping("/contacts/{id}")
+	public String deleteSpecificContact(@PathVariable int id) {
+		return service.deleteContact(id);
+	}
+	
+	@PutMapping("/contacts")
+	public String updateContact(@RequestBody Contact contact) {
+		return service.updateContact(contact);
+	}
 }
--- src/main/java/org/becode/projects/services/ContactService.java
+++ src/main/java/org/becode/projects/services/ContactService.java
@@ -1,5 +1,44 @@
 package org.becode.projects.services;
 
+import java.util.List;
+
+import org.becode.projects.domain.Company;
+import org.becode.projects.domain.Contact;
+import org.becode.projects.repositories.CompanyRepository;
+import org.becode.projects.repositories.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContactService {
 
+	@Autowired
+	private ContactRepository rep;
+	
+	public ContactService() {
+		
+	}
+	
+	public List<Contact> getAllContacts(){
+		return rep.findAll();
+	}
+	
+	public Contact getSpecificContact(int id) {
+		return rep.getById(id);
+	}
+	
+	public String createNewContact(Contact contact) {
+		rep.save(contact);
+		return "New contact successfully created";
+	}
+	
+	public String deleteContact(int id) {
+		rep.deleteById(id);
+		return String.format("Contact with id %d successfully removed", id);
+	}
+	
+	public String updateContact(Contact contact) {
+		rep.save(contact);
+		return "Contact successfully updated";
+	}
 }
