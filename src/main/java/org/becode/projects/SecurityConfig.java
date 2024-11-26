7bd16bef62a74c89716bf48b83c3fe68ebe1ad06
Robin Sanders <robin.sanders1@telenet.be>
2024-11-26 11:40:08 +0100

    Add dependency jjwt jackson, add authenticationManager in securityConfig

--- src/main/java/org/becode/projects/SecurityConfig.java
+++ src/main/java/org/becode/projects/SecurityConfig.java
@@ -3,9 +3,11 @@ package org.becode.projects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -42,6 +44,11 @@ public class SecurityConfig {
 		provider.setUserDetailsService(userDetailService);
 		return provider;
 		
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 	
 //	@Bean
